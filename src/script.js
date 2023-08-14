8fdf291e943937c9b4c970d338f9f2dc961f289d
Bharath Dubbaka <bharath.reactdev@gmail.com>
2023-08-14 07:49:06 +0530

    added objects and effects, removed helpers

--- src/script.js
+++ src/script.js
@@ -1,21 +1,22 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import * as dat from 'dat.gui';
-import gsap from "gsap"
-import {RGBELoader} from "three/examples/jsm/loaders/RGBELoader.js"
+import * as dat from "dat.gui";
+import gsap from "gsap";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 
-
-const hdrTextureURL = new URL('../img/christmas_photo_studio_07_4k.hdr', import.meta.url)
-
+const hdrTextureURL = new URL(
+  "../img/christmas_photo_studio_07_4k.hdr",
+  import.meta.url
+);
 
 // Scene
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff00ff });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff00ff });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
 
 // Sizes
 const sizes = {
@@ -23,26 +24,24 @@ const sizes = {
   height: window.innerHeight,
 };
 
-// Axis Helper
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
+// // Axis Helper
+// const axesHelper = new THREE.AxesHelper(5);
+// scene.add(axesHelper);
 
-// Grid Helper
-const gridHelper = new THREE.GridHelper(30);
-scene.add(gridHelper);
+// // Grid Helper
+// const gridHelper = new THREE.GridHelper(30);
+// scene.add(gridHelper);
 
 //SPHERE
-const sphereGeometry = new THREE.SphereGeometry(1, 50, 50);
-const sphereMaterial = new THREE.MeshBasicMaterial({
-    color: 0x9900FF,
-    wireframe: false});
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-scene.add(sphere);
-sphere.position.set(-5, 5, 5);
+// const sphereGeometry = new THREE.SphereGeometry(1, 50, 50);
+// const sphereMaterial = new THREE.MeshBasicMaterial({
+//   color: 0x9900ff,
+//   wireframe: false,
+// });
+// const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+// scene.add(sphere);
+// sphere.position.set(-5, 5, 5);
 // sphere.castShadow = true;
-
-
-
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
@@ -60,7 +59,6 @@ scene.add(camera);
 //       camera.lookAt(0,0,0)
 //     }
 //   })
-
 
 //   tl.to(camera.position, {
 //     y:14,
@@ -81,28 +79,51 @@ scene.add(camera);
 //   })
 // })
 
-
-//HDRI
-
-const loader = new RGBELoader()
-loader.load(hdrTextureURL, function (texture) {
-  texture.mapping = THREE.EquirectangularReflectionMapping;
-  scene.background = texture
-})
-
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("canvas.webgl"),
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 2.0;
 
+//HDRI
+const loader = new RGBELoader();
+loader.load(hdrTextureURL, function (texture) {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = texture;
+  scene.environment = texture;
+
+  const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 50, 50),
+    new THREE.MeshStandardMaterial({
+      roughness: 0,
+      metalness:0.5,
+      color:0xFFEA00
+    })
+  );
+  scene.add(sphere);
+  sphere.position.x = 1.5
+
+  const sphere2 = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 50, 50),
+    new THREE.MeshStandardMaterial({
+      roughness: 0,
+      metalness:0.5,
+      color:0x00FF00
+    })
+  );
+  scene.add(sphere2);
+  sphere2.position.x = -1.5
+  
+});
 //DAT.GUI
 // const gui = new dat.GUI();
 
 // const options = {
 //     sphereColor: '#ffea00'
- 
+
 // };
 
 // gui.addColor(options, 'sphereColor').onChange(function(e){
@@ -118,7 +139,6 @@ renderer.outputEncoding = THREE.sRGBEncoding;
 // gui.add(options, 'angle', 0, 1);
 // gui.add(options, 'penumbra', 0, 1);
 // gui.add(options, 'intensity', 0, 1);
-
 
 // ORBIT CONTROLS
 const orbitControls = new OrbitControls(camera, renderer.domElement);
